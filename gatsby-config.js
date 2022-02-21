1ef10b2e245554c6b3f94396852cd8ea09298f72
Jason Dorn <apollo701@gmail.com>
2022-02-21 17:41:15 -0500

    remove old templates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Pho Cure Burn",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "This is the website for the Burning Man camp, Pho Cure Burn",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix("/")}img/og-image.jpg`}
+          content={`${withPrefix("/")}img/adam-hornyak-c0wcdaxudhm-unsplash.jpg`}
         />
       </Helmet>
       <Navbar />
